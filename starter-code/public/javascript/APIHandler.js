c5882b0b029bd9fed6747ea141164ed700dff98e
Bianca Castro <biiianca_00@hotmail.com>
2018-11-21 16:10:06 +0100

    starting 2nd iteration

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,14 +1,25 @@
+
+
 class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
   getFullList () {
-
+    return axios.get(`${this.BASE_URL}/characters`)
+    .then((character)=>{
+       console.log(character.data);
+      console.log(`${this.BASE_URL}/characters`)
+       return character.data;
+    })
+    .catch(err=>{
+      console.log(err)
+    })
   }
 
   getOneRegister () {
 
+    // axios.get(`${this.BASE_URL}/characters/:id`)
   }
 
   createOneRegister () {
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,8 +1,25 @@
+
+
+
 const charactersAPI = new APIHandler("http://localhost:8000")
 
 $(document).ready( () => {
   document.getElementById('fetch-all').onclick = function(){
+    charactersAPI.getFullList()
+    .then((characters)=>{
+    let node= document.querySelector('.characters-container');
 
+    for (var i=0; i<characters.length;i++){
+
+      let characterInfo = `<div class="character-info">
+      <div class="name">${characters[i].name}</div>
+      <div class="occupation">${characters[i].occupation}</div>
+      <div class="cartoon">${characters[i].cartoon}</div>
+      <div class="weapon">${characters[i].weapon}</div>
+    </div>`
+      node.innerHTML += characterInfo;
+    }
+    });
   }
   
   document.getElementById('fetch-one').onclick = function(){
@@ -20,4 +37,5 @@ $(document).ready( () => {
   document.getElementById('new-character-form').onsubmit = function(){
                 
   }
+  charactersAPI.getFullList()
 })
